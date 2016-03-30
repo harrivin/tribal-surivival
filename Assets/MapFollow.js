0a9daae2614eb1be94d78bc091ac6ea1af891ada
tiffan3 <tiffan3@ufl.edu>
2016-03-29 23:20:48 -0400

    Added UI

---
+++ Assets/MapFollow.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var target: Transform;
+
+function Update () {
+gameObject.transform.position.x=target.transform.position.x;
+gameObject.transform.position.z=target.transform.position.z;
+
+}
---
+++ Assets/ShowMap.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+//var miniBigCam : Camera;
+var miniCam : Camera;
+var Health : GameObject;
+var Inv : GameObject;
+var Pause: GameObject;
+var wood: GameObject;
+var map: GameObject;
+//var inventory: GameObject;
+
+var close: GameObject;
+
+ 
+function Start () {
+// Enable small minimap and disable big minimap on start
+miniCam.enabled = false;
+close.active=false;
+//miniBigCam.enabled = false;
+}
+ 
+//Open and close Minimap
+function Update () {
+// I have to add double "miniBigCam.enabled = !miniBigCam.enabled;" because it's false when i click start.
+if (Input.GetKeyDown(KeyCode.M)) {
+    //miniCam.enabled = !miniCam.enabled;
+    showMap();
+}}
+
+function showMap(){
+	miniCam.enabled = !miniCam.enabled;
+	Health.active=!Health.active;
+	Inv.active=!Inv.active;
+	map.active=!map.active;
+		wood.active=!wood.active;
+
+	Pause.active=!Pause.active;
+	close.active= true;
+}
+
+function CloseMap(){
+close.active=false;
+	miniCam.enabled = !miniCam.enabled;
+	Health.active=!Health.active;
+	map.active=!map.active;
+	Inv.active=!Inv.active;
+	wood.active=!wood.active;
+
+	Pause.active=!Pause.active;
+
+}
